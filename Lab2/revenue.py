f26ad531a9657317578de4d857f5b862ee2bd1a1
mnosel00 <mateusz.nosel@student.pk.edu.pl>
2026-03-18 08:16:51 +0100

    comm

--- Lab2/revenue.py
+++ Lab2/revenue.py
@@ -11,18 +11,10 @@ columns = [
     "Taker buy base asset volume", "Taker buy quote asset volume", "Ignore"
 ]
 
-# Wczytanie danych
 df = pd.read_csv(file_path, names=columns)
 
-# Zostawiamy tylko kolumnę z ceną zamknięcia
 df_btc = df[['Close']].copy()
 
-# Obliczenie logarytmicznych stóp zwrotu
-# shift(1) bierze cenę z poprzedniej minuty (P(t-1))
 df_btc['Log_Return'] = np.log(df_btc['Close']) - np.log(df_btc['Close'].shift(1))
 
-# Usunięcie pierwszego wiersza (nie da się policzyć zwrotu dla pierwszej minuty, bo nie ma danych historycznych)
 df_btc = df_btc.dropna()
-
-# Wyświetlenie pierwszych 5 wierszy
-print(df_btc.head())
